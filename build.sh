5000d899a0ba7b40780a2108e61c74226d7ac1a4
Siddhant Sarkar <siddhantsarkar2404@gmail.com>
2018-11-10 14:01:39 +0530

    Building App

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-export ANDROID_HOME="/home/sahil/Android/Sdk"
+export ANDROID_HOME="/home/siddhant/Android/Sdk"
 export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 ionic cordova build android
 cp platforms/android/app/build/outputs/apk/debug/app-debug.apk app.apk
